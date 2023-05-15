b4711df3619391d953c7ad4fed75996f973afe3e
Lucas <lucasprogjr@gmail.com>
2023-05-15 20:53:55 -0300

    feat: add test component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { useFetch } from "./hooks/useFetch";
 
-function App() {
-  const resposta = useFetch(
-    "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=pt-Br&page=1&sort_by=popularity.desc&api_key="
+export default function App() {
+  const data = useFetch(
+    "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key="
   );
-  const imagePath = "https://image.tmdb.org/t/p/original/";
-  const dadosFilmes = resposta[0];
-  console.log(dadosFilmes);
-
-  return <></>;
+  const imgPath = "https://image.tmdb.org/t/p/original";
+  return (
+    <>
+      <p>oi</p>
+      {data.map((data) => {
+        return (
+          <div key={data.id}>
+            <img src={imgPath + data.poster_path} alt="" />
+            <p>{data.title}</p>
+          </div>
+        );
+      })}
+    </>
+  );
 }
-
-export default App;
