5a73b718d7f03cc0969f83b8131a14b8abe2dd99
Lucas <lucasprogjr@gmail.com>
2023-05-16 20:35:31 -0300

    feat: implementa a verificação do estado da requisição

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -3,13 +3,17 @@ import { useEffect, useState } from "react";
 
 export function useFetch(url) {
   const [data, setData] = useState(0);
+  const [isFething, setIsFething] = useState(true);
   const keySecret = "7a887acb2ac607766d92d62c035c4c09";
 
   useEffect(() => {
-    axios.get(url + keySecret).then((response) => {
-      setData(response.data.results);
-    });
+    axios
+      .get(url + keySecret)
+      .then((response) => {
+        setData(response.data.results);
+      })
+      .finally(setIsFething(false));
   }, [url]);
 
-  return data;
+  return { data, isFething };
 }
