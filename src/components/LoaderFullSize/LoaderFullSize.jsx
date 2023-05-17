a50c352c10afc7af6e1e313d1601d746d532bbe4
Lucas <lucasprogjr@gmail.com>
2023-05-16 21:14:59 -0300

    Feat: Create a LoaderFullSize component

---
+++ src/components/LoaderFullSize/LoaderFullSize.jsx
@@ -0,0 +1,28 @@
+import { Flex, Text, Spinner } from "@chakra-ui/react";
+
+export function LoaderFullSize() {
+  return (
+    <>
+      <Flex
+        bgColor={"#44475A"}
+        position={"fixed"}
+        width={"100vw"}
+        height={"100vh"}
+        justify={"center"}
+        align={"center"}
+      >
+        <Flex align={"center"}>
+          <Spinner
+            thickness="4px"
+            color="gray.100"
+            size="xl"
+            marginRight={10}
+          />
+          <Text color="gray.300" fontSize="xl">
+            Carregando dados
+          </Text>
+        </Flex>
+      </Flex>
+    </>
+  );
+}
