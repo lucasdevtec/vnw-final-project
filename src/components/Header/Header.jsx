2eef8c048503b2d0909a60ab37660bb2f8e07ab8
Lucas Gabriel <lucasprogjr@gmail.com>
2023-05-23 21:57:50 -0300

    02 create header (#12)
    * feat: create and test Header Component
    
    * chore: Add ajuda.css ao gitignore and delete trash
    
    * fix: Lupa

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,28 @@
+import img_da_header from "../../assets/headerImg.svg";
+import lupa from "../../assets/icons8-lupa.svg";
+import { HeaderPage, Div1, Div2, A1 } from "./style";
+
+export default function Header() {
+  return (
+    <HeaderPage>
+      <img src={img_da_header} alt="" />
+      <nav>
+        <ul>
+          <li>
+            <a href="#series">Séries</a>
+          </li>
+          <li>
+            <A1 href="#filmes">Filmes</A1>
+          </li>
+        </ul>
+      </nav>
+      <Div1>
+        <img src={lupa} />
+        <Div2>
+          <a href="#filmes">Login</a>
+          <a href="#series">Filtro</a>
+        </Div2>
+      </Div1>
+    </HeaderPage>
+  );
+}
