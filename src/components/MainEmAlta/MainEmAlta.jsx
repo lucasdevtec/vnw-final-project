d17ed67ccf92dcf19bd090dad3625bb32da55ddb
Lucas <lucasprogjr@gmail.com>
2023-05-23 21:02:52 -0300

    style: Estilizando o componente

--- src/components/MainEmAlta/MainEmAlta.jsx
+++ src/components/MainEmAlta/MainEmAlta.jsx
@@ -1,30 +1,40 @@
 import LoaderFullSize from "../LoaderFullSize/LoaderFullSize";
+import {
+  Conteiner,
+  ConteinerItemImg,
+  ConteinerItem,
+  ConteinerGrid,
+} from "./style";
 import { useFetch } from "../../hooks/useFetch";
 
 export default function MainEmAlta() {
   const data = useFetch(
-    "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key="
+    "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=pt-br&page=1&sort_by=popularity.desc&api_key="
   );
   const movies = data.data;
   const imgPath = "https://image.tmdb.org/t/p/original";
   return (
     <>
       {data.isFething === true && <LoaderFullSize />}
-      <p>oi</p>
-      <div>
-        {movies.length > 0 &&
-          movies.slice(0, 10).map((data) => {
-            return (
-              <div key={data.id}>
-                <div>
-                  <img src={imgPath + data.poster_path} alt="" />
-                </div>
-                <h2>{data.title}</h2>
-                <p>{data.release_date.slice(0, 4)}</p>
-              </div>
-            );
-          })}
-      </div>
+      <Conteiner>
+        <h2>Em alta</h2>
+        <ConteinerGrid>
+          {movies.length > 0 &&
+            movies.slice(0, 10).map((data) => {
+              return (
+                <ConteinerItem onClick={console.log("click")} key={data.id}>
+                  <ConteinerItemImg>
+                    <img src={imgPath + data.poster_path} alt="" />
+                  </ConteinerItemImg>
+                  <h3>
+                    <strong>{data.title}</strong>
+                  </h3>
+                  <p>{data.release_date.slice(0, 4)}</p>
+                </ConteinerItem>
+              );
+            })}
+        </ConteinerGrid>
+      </Conteiner>
     </>
   );
 }
---
+++ src/components/MainEmAlta/style.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const Conteiner = styled.section`
+  padding: 10px;
+
+  h2 {
+    font-size: 25px;
+    font-height: bold;
+    margin-left: 70px;
+  }
+`;
+
+export const ConteinerGrid = styled.div`
+  display: grid;
+  margin-top: 10px;
+  justify-items: center;
+  align-items: center;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+`;
+
+export const ConteinerItem = styled.div`
+  width: 170px;
+  height: 350px;
+`;
+
+export const ConteinerItemImg = styled.div``;
