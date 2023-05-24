d8e1297f9603a237642cd998725261cfd4e21916
Lucas <lucasprogjr@gmail.com>
2023-05-24 19:26:44 -0300

    style: add background black

--- src/components/MainEmAlta/style.js
+++ src/components/MainEmAlta/style.js
@@ -2,11 +2,13 @@ import styled from "styled-components";
 
 export const Conteiner = styled.section`
   padding: 10px;
+  background: black;
 
   h2 {
     font-size: 25px;
     font-height: bold;
     margin-left: 70px;
+    color: #ffffff;
   }
 `;
 
@@ -21,6 +23,7 @@ export const ConteinerGrid = styled.div`
 export const ConteinerItem = styled.div`
   width: 170px;
   height: 350px;
+  color: #f6f6f6;
 `;
 
 export const ConteinerItemImg = styled.div``;
