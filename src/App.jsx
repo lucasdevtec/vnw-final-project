47c3521b079f13a90cddbf9f4bb65765f95efa8c
Lucas <lucasprogjr@gmail.com>
2023-05-24 19:25:41 -0300

    style: create global style

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
+import { GlobalStyle } from "./GlobalStyle";
 
 export default function App() {
   return (
     <>
       <Header />
       <Main />
+      <GlobalStyle />
     </>
   );
 }
