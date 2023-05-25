df81242c90eddfd9670f6a8a987159f0253403bc
Lucas <lucasprogjr@gmail.com>
2023-05-25 20:47:47 -0300

    Feat: Create component MainDestaque

---
+++ src/components/MainDestaque/MainDestaque.jsx
@@ -0,0 +1,14 @@
+import { Conteiner } from "./style";
+
+export default function MainDestaque() {
+  return (
+    <>
+      <Conteiner>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </Conteiner>
+    </>
+  );
+}
