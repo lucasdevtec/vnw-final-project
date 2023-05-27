4ae909436e72e4c64cb07d86df5e38bee355739a
Lucas <lucasprogjr@gmail.com>
2023-05-26 21:47:45 -0300

    feat: add buttons and texts on this component.

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,12 @@
 import MainCarrousel from "../MainCarrousel/MainCarrousel";
+import MainDestaque from "../MainDestaque/MainDestaque";
 import MainEmAlta from "../MainEmAlta/MainEmAlta";
 import NavGeneros from "../NavGeneros/NavGeneros";
 
 export default function Main() {
   return (
     <main>
+      <MainDestaque />
       <NavGeneros />
       <MainCarrousel />
       <MainEmAlta />
